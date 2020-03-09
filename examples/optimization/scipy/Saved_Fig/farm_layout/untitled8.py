e6d4c5960c9ef04abe493ffa726efa6c41a910f9
DavidBensason <david.bensason@nrel.gov>
2020-03-09 11:12:13 -0600

    Saved folders

---
+++ examples/optimization/scipy/Saved_Fig/farm_layout/untitled8.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 11:10:24 2020
+
+@author: dbensaso
+"""
+
---
+++ examples/optimization/scipy/Saved_Fig/ti_rose/untitled11.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 11:10:59 2020
+
+@author: dbensaso
+"""
+
